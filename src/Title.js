f017332792527c5490c1971761ba47cdda5f191f
affyzal <32396442+affyzal@users.noreply.github.com>
2021-04-05 13:57:14 +0100

    title

---
+++ src/Title.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Title.css'
+
+function Title() {
+    return (
+        <div>
+
+            <div className="Title">
+                <h1>A Peek Into the Presidential Debates</h1>
+            </div>
+
+            <div className="Title-Subtitle">
+                <h2>A Sentiment Analysis View of the Presidential Debates</h2>
+            </div>
+
+        </div>
+
+)
+}
+
+export default Title
