e43f95a1305d1f06a27441548c30826d7194d6d1
affyzal <32396442+affyzal@users.noreply.github.com>
2021-04-06 23:05:34 +0100

    analysis homepage. analysis skeletons

--- src/HomeBar.js
+++ src/HomeBar.js
@@ -13,31 +13,75 @@ import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
 import ThreeDRotation from '@material-ui/icons/ThreeDRotation';
 import HomeIcon from '@material-ui/icons/Home';
 import { BrowserRouter, Route, Link } from "react-router-dom";
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Switch from '@material-ui/core/Switch';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
 
 export default function ButtonAppBar() {
+    const [auth, setAuth] = React.useState(true);
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
 
+    const handleChange = (event) => {
+        setAuth(event.target.checked);
+    };
+
+    const handleMenu = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
     return (
         <div>
             <AppBar position="static" color="secondary">
                 <Toolbar>
+                    <div>
+                        <IconButton
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorEl}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={open}
+                            onClose={handleClose}
+                        >
+                            <MenuItem onClick={handleClose} component={Link}   to="/sentiment">Sentiment Analysis</MenuItem>
+                            <MenuItem onClick={handleClose} component={Link}   to="/sentence">Sentence Analysis</MenuItem>
+                            <MenuItem onClick={handleClose} component={Link}   to="/word">WordAnalysis</MenuItem>
+                            <MenuItem onClick={handleClose} component={Link}   to="/heatmaps">HeatMaps</MenuItem>
+
+                        </Menu>
+                    </div>
                     <Grid
                         container
                         direction="column"
                         justify="space-between"
                         alignItems="flex-start">
-                        <Grid item container>
-                            <Grid item xs={11}>
-                                <Typography variant="h6" >
-                                    Argumentation and Analysis Based Infographics concerning the Presidential Debates
-                                </Typography>
-                            </Grid>
-                            <Grid item xs={1}>
-                                <Link to="/">
-                                    <HomeIcon fontSize="large" />
-                                </Link>
-                            </Grid><
-                        /Grid>
+                        <Typography variant="h6" >
+                            Argumentation and Analysis Based Infographics concerning the Presidential Debates
+                        </Typography>
                     </Grid>
+                    <Link to="/">
+                            <HomeIcon fontSize="large" />
+                        </Link>
                 </Toolbar>
             </AppBar>
         </div>
